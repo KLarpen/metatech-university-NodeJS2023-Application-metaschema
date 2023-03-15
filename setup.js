2ae2dfc74ac087b8b38a46abe843683a3e98850c
Vladyslav Karpenko <klarpen@gmail.com>
2023-03-15 09:29:51 +0200

    Cleanup environment setup script

--- setup.js
+++ setup.js
@@ -6,6 +6,19 @@ const pg = require('pg');
 const metasql = require('metasql');
 const config = require('./config.js');
 
+/**
+ * Default PostgreSQL main user with permissions to DROP/CREATE database & user
+ * Host and port are commented because in real environment they might be modified
+ * what will be represented in application config.
+ */
+const GENERIC_CONNECTION = {
+  // host: '127.0.0.1',
+  // port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'postgres',
+};
+
 const DB_DIR = path.join(process.cwd(), './db');
 const SCHEMAS_DIR = path.join(process.cwd(), './schemas');
 
@@ -41,23 +54,18 @@ const executeFile = async (client, name) => {
   const domainTypes = path.join(DB_DIR, 'domain.d.ts');
   await fsp.rename(typesFile, domainTypes);
 
-  // (Re)Create database & user by executing `./db/install.sql` script
   const inst = new pg.Client({
     ...config.DB,
-    // Default PostgreSQL main user with permissions to DROP/CREATE database & user
-    database: 'postgres',
-    user: 'postgres',
-    password: 'postgres',
+    ...GENERIC_CONNECTION,
   });
   await inst.connect();
   console.log('(Re)Create database & user');
   await executeFile(inst, 'install.sql');
   await inst.end();
 
-  // Setup database structure & seed it with data
   const db = new pg.Client(config.DB);
   await db.connect();
-  console.log('Instantiate database structure');
+  console.log('Setup database structure');
   await executeFile(db, 'structure.sql');
   console.log('Database seeding');
   await executeFile(db, 'data.sql');
